0a4d0234cee181ffa21e7dde05b2e29bd5f2a557
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-04-07 14:52:46 -0600

    documnet ready set up
    Co-authored-by: Ryan Walker <ryandanielwalker@gmail.com>
    Co-authored-by: James Wyn <thisisjameswyn@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
-function PlayerObject(playerName, totalScore, tempScore) {
+function PlayerObject(playerName) {
   this.playerName = playerName;
-  this.totalScore = totalScore;
-  this.tempScore = tempScore;
+  let totalScore = 0;
+  let tempScore = 0;
 }
 
 function switchPlayer() {
-  if (currentPlayer === playerOne) {
+  if (currentPlayer.playerName === "playerOne") {
     currentPlayer = playerTwo;
   } else {
     currentPlayer = playerOne;
@@ -17,13 +17,21 @@ function switchPlayer() {
 PlayerObject.prototype.rollDice = function () {
   let currentRoll = Math.floor(Math.random() * 6) + 1 //random number
   if (currentRoll === 1) { //return if 1
-    return totalScore;
+    switchPlayer();
   } else {
     this.tempScore += currentRoll; //add if !one
     let myChoice = prompt("Would you like to roll again?")
     if (myChoice) {
-      rolldice
+      currentPlayer.rollDice();
     } else {
-      totaSCore += tempScore;
-      playerSwitch;
+      this.totalScore += this.tempScore;
+      switchPlayer();
     }
+  }
+}
+
+$(document).ready(function () {
+  const playerOne = new PlayerObject("playerOne");
+  const playerTwo = new PlayerObject("playerTwo");
+  console.log("I'm running");
+});
