ebe3d15294ae77af4eb96acac67d8f3e858a0422
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-04-07 15:48:36 -0600

    add fucntion for player one and documentation for testing
    Co-authored-by: Ryan Walker <ryandanielwalker@gmail.com>
    Co-authored-by: James Wyn <thisisjameswyn@gmail.com>

---
+++ README.md
@@ -0,0 +1,4 @@
+let player1 = new PlayerObject("brent")
+undefined
+player1
+PlayerObject {playerName: "brent", totalScore: undefined, tempScore: undefined}
--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
+let playerOne;
+let playerTwo;
+let currentPlayer;
+
 function PlayerObject(playerName) {
   this.playerName = playerName;
-  let totalScore = 0;
-  let tempScore = 0;
+  this.totalScore = 0;
+  this.tempScore = 0;
 }
-
+//player1
+//totalScore = 0
+//tempScore = 0
+//player2
+//totalScore = 0
+//tempScore = 0
 function switchPlayer() {
   if (currentPlayer.playerName === "playerOne") {
     currentPlayer = playerTwo;
@@ -13,15 +22,18 @@ function switchPlayer() {
   currentPlayer.tempScore = 0;
 }
 
-
+//player1.rollDice()
 PlayerObject.prototype.rollDice = function () {
   let currentRoll = Math.floor(Math.random() * 6) + 1 //random number
   if (currentRoll === 1) { //return if 1
+    alert("You busted!");
+    console.log("You busted!");
     switchPlayer();
   } else {
     this.tempScore += currentRoll; //add if !one
     let myChoice = prompt("Would you like to roll again?")
     if (myChoice) {
+      console.log(currentPlayer);
       currentPlayer.rollDice();
     } else {
       this.totalScore += this.tempScore;
@@ -30,8 +42,10 @@ PlayerObject.prototype.rollDice = function () {
   }
 }
 
+
 $(document).ready(function () {
-  const playerOne = new PlayerObject("playerOne");
-  const playerTwo = new PlayerObject("playerTwo");
-  console.log("I'm running");
+  playerOne = new PlayerObject("playerOne");
+  playerTwo = new PlayerObject("playerTwo");
+  currentPlayer = playerOne;
+  console.log(currentPlayer);
 });
